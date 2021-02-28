f22c83b0c72c71cb29798adce7c7059fa019f5cb
root <root@pi-gen.pitowers.org>
2021-02-27 23:08:33 -0800

    attempted fseek but still need to test it by reading out the struct strings from the file

--- recfile.c
+++ recfile.c
@@ -1,4 +1,7 @@
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
@@ -15,8 +18,10 @@ int main() {
   int errnum = errno;
   int nread; // number of bytes read by read()
   FILE* fp;
-  char recordNumber[128]; // first user input captured - record they want to change
-  char newNumber[128]; // second user input captured - record they want to change
+  char recordNumberBuffer[128]; // first user input captured - record they want to change
+  int recordNumber = 0;
+  char newNumberBuffer[128]; // second user input captured - record they want to change
+  int newNumber = 0;
 
   fp = fopen("records.dat", "w");
   
@@ -34,37 +39,60 @@ int main() {
 
     // assign values to RECORD struct instance
     currRecord.integer = i; // assign number to record
-    sprintf(currRecord.string, "RECORD-%d\n", i); // assign string to record
+    sprintf(currRecord.string, "RECORD-%d", i); // assign string to record
     // write out current record to records.dat
     fwrite(&currRecord, sizeof(RECORD), 100, fp);
   }
 
   printf("Success: Finished populating records.dat\n");
+  fclose(fp); // close file to save
+
   // capture first user input - record number want to change
   printf("Enter the record number you want to update: \n");
   // input error handling
-  if(fgets(recordNumber, sizeof(recordNumber), stdin) == NULL){
+  if(fgets(recordNumberBuffer, sizeof(recordNumberBuffer), stdin) == NULL){
     printf("ERROR: User input was not successfully collected\n");
     exit(EXIT_FAILURE);
   }
   // collect user input
   else{
-    printf("SUCCESS: Input captured\nYou entered: ");
-    puts(recordNumber);
+    sscanf(recordNumberBuffer, "%d", &recordNumber);
+    printf("Successfully captured input.\nYou entered %d\n", recordNumber);
   }
   // capture second user input - number want to assign to record
   printf("Enter the new number you want to assign the record: \n");
   // input error handling
-  if(fgets(newNumber, sizeof(newNumber), stdin) == NULL){
+  if(fgets(newNumberBuffer, sizeof(newNumberBuffer), stdin) == NULL){
     printf("ERROR: User input was not successfully collected\n");
   }
   // collect user input
   else{
-    printf("SUCCESS: Input captured\nYou entered: ");
-    puts(newNumber);
+    sscanf(newNumberBuffer, "%d", &newNumber);
+    printf("Successfully captured input.\nYou entered: %d\n", newNumber);
   }
 
+  fp = fopen("record.dat", "w+");
+  RECORD newRecord;
+  newRecord.integer = newNumber;
+  sprintf(newRecord.string, "RECORD-%d", newNumber);
+
+  //fseek(fp, recordNumber, SEEK_SET);
+  fseek(fp, recordNumber * sizeof(RECORD), SEEK_SET);
+  fwrite(&newRecord, sizeof(RECORD), 1, fp);   
+
   fclose(fp);
+
+  fp = fopen("records.dat", "r");
+
+  char currentString[8];
+  RECORD loopRecord;
+  int count = 0;
+  while(fread(&loopRecord, sizeof(RECORD), 1, fp)){
+    printf("%s\n", loopRecord.string);
+    ++count;
+  }
+
+  printf("%i\n", count);
 
   exit(EXIT_SUCCESS);
 }
